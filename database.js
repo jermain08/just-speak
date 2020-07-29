f5b47c7e1e1e8a7ade70a3dc30f4a1887676abca
minhkl <lkminh.uns@gmail.com>
2020-07-29 11:37:54 +0700

    add docker-compose

--- database.js
+++ database.js
@@ -1,10 +1,11 @@
 
 var mysql = require('mysql');
 var connection = mysql.createConnection({
-  host: 'localhost',
-  user: 'root',
-  password: 'RootRoot',
-  database: 'gospeak',
+  host: '172.20.0.2',
+  user: 'minhkl',
+  password: 'password',
+  database: 'justspeakdb',
+  port: '3306',
   insecureAuth: true
 });
 
