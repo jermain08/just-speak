3e4f2a3abb019a22f64dcc3487dd140ec76cece5
minhkl <lkminh.uns@gmail.com>
2020-07-30 22:28:14 +0700

    feat: add .env

--- database.js
+++ database.js
@@ -1,11 +1,14 @@
 
 var mysql = require('mysql');
+var dotenv = require('dotenv');
+dotenv.config();
+
 var connection = mysql.createConnection({
-  host: '172.20.0.2',
-  user: 'minhkl',
-  password: 'password',
-  database: 'justspeakdb',
-  port: '3306',
+  host: process.env.DATABASE_HOST,
+  user: process.env.DATABASE_USER,
+  password: process.env.DATABASE_PASSWORD,
+  database: process.env.DATABASE_NAME,
+  port: process.env.DATABASE_PORT,
   insecureAuth: true
 });
 
