cbe0be51640164405d06f4fa35015a0c025daa61
jermain08 <lkminh.uns@gmail.com>
2020-08-16 22:53:22 +0700

    Authentication (#2)
    * add responseFail & responseSuccess helpers
    
    * apply jwt
    
    * handle invalid token error

--- app.js
+++ app.js
@@ -1,11 +1,16 @@
 var express = require('express');
 var path = require('path');
+var jwt = require('express-jwt');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var sentencesRouter = require('./routes/sentences');
+var authRouter = require('./routes/auth');
+var { responseFail } = require('./utils/helpers');
+const { networkInterfaces } = require('os');
+const { nextTick } = require('process');
 var app = express();
 
 app.use(logger('dev'));
@@ -14,7 +19,16 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use(jwt({ secret: process.env.JWT_SECRET, algorithms: ['HS256'] }).unless({ path: ['/auth'] }));
+app.use(function (err, req, res, next) {
+  if (err && err.name === 'UnauthorizedError') {
+    return responseFail(res, "Invalid Token", 401);
+  }
+  next();
+});
+
 app.use('/', indexRouter);
+app.use('/auth', authRouter);
 app.use('/users', usersRouter);
 app.use('/sentences', sentencesRouter);
 
--- routes/sentences.js
+++ routes/sentences.js
@@ -1,24 +1,16 @@
 var express = require('express');
-var { to } = require('await-to-js');
-
-var database = require('../database');
+var { to } = require('../utils/helpers');
 var { Sentence, Pattern } = require('../models');
+var { responseSuccess, responseFail } = require('../utils/helpers');
 var router = express.Router();
 
-/* GET users listing. */
+/* GET sentences listing. */
 router.get('/', async function (req, res, next) {
-  const [error, sentences] = await to(Sentence.findAll({ include: { model: Pattern, as: 'pattesrn' }, }));
-
+  const [error, sentences] = await to(Sentence.findAll({ include: { model: Pattern, as: 'pattern' } }));
   if (error) {
-    return res.status(500).json({
-      success: false,
-      error: error,
-    })
+    return responseFail(res, error);
   }
-  return res.status(200).json({
-    success: true,
-    sentences: sentences,
-  });
+  return responseSuccess(res, sentences);
 });
 
 module.exports = router;
